d5e655a564ad92d121ab4500e7024f52e341e08e
kickeyhickey <irishmanhickey@gmail.com>
2021-12-05 16:28:48 -0800

    CHickey completed assessment for week 1

--- code-challenges.js
+++ code-challenges.js
@@ -6,12 +6,33 @@
 // If you get stuck, please leave comments to help us understand your thought process.
 
 // --------------------1) Create a statement that determines if a temperature is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit.
+var temp = 216
+
+if(temp < 212){
+    console.log(`The temperature is ${temp}, which is below the boiling point`); // logs temp is less than boiling point
+} else if(temp > 212){
+    console.log(`The temperature is ${temp}, which is above the boiling point`); // logs temp is more than boiling point
+} else{
+    console.log(`The temperature is ${temp}, which is at the boiling point`);
+} // logs the temp is equal to the boiling point
+
+// -----NOTE * Since this problem had 3 different outcomes, I used an if else statement. I used the less than greater than operators to allow for any temperature to be given. I tried template literals to help reflect what temp was input by the end user.
+
+
 
 // Create the code and test each of the variables provided. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
-var temp = 42
 // var temp = 350
-// var temp = 212
+// // var temp = 212
+var temp = 42
+if(temp === 42){
+    console.log("42 is below the boiling point");
+} else if(temp === 350){
+    console.log("350 is above the boiling point");
+} else if(temp === 212){
+console.log("212 is at the boiling point");
+}
+//-------NOTE * Piggy backing off the first code, I change the parameters to be more specific to the given variables by using the ===. 
 
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
@@ -19,10 +40,20 @@ var temp = 42
 var myNumbers1 = [3, 7, 0, 36, -9]
 var myNumbers2 = [8, -7, 42, 9, 13]
 
+var combined = myNumbers1.concat(myNumbers2)
+// console.log(combined);
+console.log(combined.length);
+//------- NOTE * On this problem, I simply reasoned that since I was already given you two variables, that a third one would be needed to hold them both.
+
 
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "1202 ohcE"
 
+
 var currentCohort = "Echo 2021"
+
+var reversed = currentCohort.split("")
+console.log(reversed.reverse().join(""));
+//-------NOTE * This problem gave me some trouble! I didn't realize that two built in methods could be combined like this. I saw this format while researching another problem and gave it a try. I was excited to see the answer pop up.
 
 
 // --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
@@ -30,10 +61,31 @@ var currentCohort = "Echo 2021"
 var myArray = [13, 34, 5, 10, 27, 42]
 
 
-// --------------------5) Create a statement that evaluates two numbers and subtracts the smaller number from the larger number using the two sets of test variables provided below. Expected output: 42, 3
+for(let i = 0; i < myArray.length; i++){
+  if(myArray[i] % 2 === 0){
+      console.log("even");
+  }else{
+      console.log("odd");
+  }
+}
+// -------NOTE * This problem took me a long while to figure out. I had to play and play with it. Saying out loud line by line what the formatting in the "for" method did and represented. Finally I tried myArray[i] as a guess and found a happy outcome.
 
-var number1 = 58
-var number2 = 100
+// // --------------------5) Create a statement that evaluates two numbers and subtracts the smaller number from the larger number using the two sets of test variables provided below. Expected output: 42, 3
+
+// var number1 = 58
+// var number2 = 100
 
 // var number1 = 27
 // var number2 = 24
+
+var number1 = 27
+var number2 = 27
+
+if(number1 < number2){
+    console.log(number2 - number1)
+} else if(number1 > number2){
+    console.log(number1 - number2)
+} else{
+    console.log("The numbers can not be equal!");
+}
+// ------NOTE * This problem was straight forward with me. I knew 3 outcomes would be needed so an "if else" state would be needed. I created additional variables that were equal to help the outcome if the user input two numbers that were equal.
--- coding-concepts.js
+++ coding-concepts.js
@@ -10,8 +10,8 @@
 var cohort = "Echo 2021"
 // console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: The output will be 9
+// b) Verify and explain: I was correct. The .length method simply gives you the character length of a string.
 
 
 // --------------------2) What will this log?
@@ -19,8 +19,8 @@ var cohort = "Echo 2021"
 var greeting = "Hello World!"
 // console.log(greeting[3])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: The outcome will be "l". 
+// b) Verify and explain:  The [3] next to the varable is logging the string but only a specific character in that string. In this case the third index which is the second "l" in "hello"
 
 
 // --------------------3) What will this log?
@@ -29,8 +29,8 @@ var languages = ["JavaScript", "Ruby", "Python", "C++"]
 var index = 1
 // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: I believe it will return the string in the index [1] position in the languages array. Specifically "Ruby". 
+// b) Verify and explain: languages[1] = languages[index]. This is because in this case "index" is a varable that holds the value of of 1.
 
 
 // --------------------4) What will this log?
@@ -38,14 +38,14 @@ var index = 1
 var weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This will return a value of ["SATURDAY", "SUNDAY"]
+// b) Verify and explain: I was wrong! It slipped my mind that this method doesn't work on arrays in this way. It is a string function.
 
 
 // --------------------5) What will this log?
 
 var dataTypes = ["number", "string", "Boolean"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: It will return "string", "string", "string"
+// b) Verify and explain: I was wrong. It returned "number". I didn't immediatly make the connection that .length returns a number, therefore, the "typeof" function would be looking at the .length outcome. Then again, console.log(typeof dataTypes) game me back "object". Still struggling with teh difference between object vs string. Will be researching this more. 
